8b20198b5ab4cdce60fed25a60a9ccfbb634e9f2
Elite Carlson <elitecarlson@gmail.com>
2023-03-28 12:18:37 +0200

    more forms on signup plus form validation

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,8 +1,13 @@
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUserAuth } from "../Context/AuthContext";
+import { db } from "../firebase";
 
 export default function Index(){
+    const [firstname, setFirstname] = useState("");
+    const [secondname, setSecondname] = useState("");
+    const [username, setUsername] = useState("");
     const [emailSignup, setEmailSignup] = useState("");
     const [passwordSignup, setPasswordSignup] = useState("");
     const [emailLogin, setEmailLogin] = useState("");
@@ -12,12 +17,27 @@ export default function Index(){
     const navigation = useNavigate();
     const handleSignUp = async (e) => {
         e.preventDefault();
-        try{
-            await signUp(emailSignup, passwordSignup);
-            navigation("/home")
-        } 
-        catch(error) {
-            alert(error.message);
+        if (firstname != "" && secondname != "" && username != "" && emailSignup != "" && passwordSignup != "") {
+            try{
+                const usersnameRef = doc(db, "users", username);
+                const usernameState = await getDoc(usersnameRef);
+                if (usernameState.exists()) {
+                    alert("Username is taken")
+                } else {
+                    await signUp(emailSignup, passwordSignup);
+                    await setDoc(doc(db, "users", username), {
+                        firstname: firstname,
+                        secondname: secondname,
+                        email: emailSignup
+                    });
+                    navigation("/home")
+                }
+            } 
+            catch(error) {
+                alert(error.message);
+            }
+        } else{
+            alert("PLease fill all inputs");
         }
     }
     const handleLogin = async (e) => {
@@ -33,14 +53,17 @@ export default function Index(){
     return(
         <>
         <h3>Sign Up</h3>
-        <input type="text" placeholder="Email" onChange={(e) => setEmailSignup(e.target.value)}/>
-        <input type="text" placeholder="Password" onChange={(e) => setPasswordSignup(e.target.value)}/>
+        <input type="text" placeholder="FirstName" onChange={(e) => setFirstname(e.target.value)}/><br/><br/>
+        <input type="text" placeholder="SecondName" onChange={(e) => setSecondname(e.target.value)}/><br/><br/>
+        <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/><br/><br/>
+        <input type="text" placeholder="Email" onChange={(e) => setEmailSignup(e.target.value)}/><br/><br/>
+        <input type="text" placeholder="Password" onChange={(e) => setPasswordSignup(e.target.value)}/><br/><br/>
         <input type="submit" onClick={handleSignUp}/>
         <br /><br /><br />
 
         <h3>Log In</h3>
-        <input type="text" placeholder="Email" onChange={(e) => setEmailLogin(e.target.value)}/>
-        <input type="text" placeholder="Password" onChange={(e) => setPasswordLogin(e.target.value)}/>
+        <input type="text" placeholder="Email" onChange={(e) => setEmailLogin(e.target.value)}/><br/><br/>
+        <input type="text" placeholder="Password" onChange={(e) => setPasswordLogin(e.target.value)}/><br/><br/>
         <input type="submit" onClick={handleLogin}/>
         </>
     );
