12383b24dd18e91f089bb64aa40235beed6cffaa
Elite Carlson <elitecarlson@gmail.com>
2023-03-28 19:18:52 +0200

    Protected route bug fix

--- src/Context/ProtectedRoute.js
+++ src/Context/ProtectedRoute.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useUserAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-let { user } = useUserAuth();
-if (!user) {
-    return <Navigate to="/" />
-}
-return children;
+    const navigate = useNavigate();
+    let { user } = useUserAuth();
+    if (!user) {
+        navigate("/");
+    }
+    return children;
 }
 export default ProtectedRoute;
