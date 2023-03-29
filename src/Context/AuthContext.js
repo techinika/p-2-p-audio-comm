7176526d969e0320c4e832d3fa968694e08a4f96
Elite Carlson <elitecarlson@gmail.com>
2023-03-29 11:15:40 +0200

    add displayName

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,13 +1,19 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
 
 const AuthContext = createContext();
 export function AuthContextProvider({ children }){
     const[user, setUser] = useState("");
     // Signup function
-    function signUp(email, password){
-        return createUserWithEmailAndPassword(auth, email, password);
+    function signUp(email, password, username){
+        return createUserWithEmailAndPassword(auth, email, password)
+        .then((res) => {
+            const currentUser = auth.currentUser;
+              updateProfile(currentUser, {
+                displayName: username
+              })
+        })
     }
     // Login function
     function logIn(email, password){
