4c375a75eb0be27c2e57153b3a9c9b4e627a3178
Elite Carlson <elitecarlson@gmail.com>
2023-04-06 15:02:52 +0200

    routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./Context/AuthContext";
 import ProtectedRoute from "./Components/ProtectedRoute";
-import Index from "./Pages";
 import Home from "./Pages/Home";
-import Room from "./Pages/room";
 import Decoy from "./Pages/decoy";
+import Index from "./Pages/Index";
+import Room from "./Pages/Room";
+import Account from "./Pages/Account";
 function App() {
   return (
     <>
     <AuthContextProvider>
       <Routes>
+        <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}></Route>
         <Route path="/join" element={<Index/>}></Route>
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Home/>
-          </ProtectedRoute>}
-        ></Route>
+        <Route path="/u/:username" element={<ProtectedRoute><Account/></ProtectedRoute>}></Route>
         <Route path="/:roomID" element={<Room/>}></Route>
       </Routes>
     </AuthContextProvider>
