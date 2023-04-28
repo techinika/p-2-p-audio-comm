1ad40f37c0137df124e85faa54e3aba69aa07d70
Elite Carlson <elitecarlson@gmail.com>
2023-04-28 03:22:56 +0200

    feat: removed app.listen error handling

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,6 @@ dotenv.config();
 app.use('/api/produce', producerRoutes);
 
 const port = process.env.PORT || 6969;
-app.listen(port, (err) => {
-    if(err) console.log(err);
+app.listen(port, () => {
     console.log(`Server started on port: ${port}`)
 });
